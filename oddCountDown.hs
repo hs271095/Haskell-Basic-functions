b0b07036b84e4bff695534f08f09d2f2716dfd66
Harshit <hs271095@gmail.com>
2019-09-23 02:10:28 -0400

    updates oddCountDown.hs

--- oddCountDown.hs
+++ oddCountDown.hs
@@ -1,3 +1,7 @@
 oddCountDown :: Int -> [Int]
-oddCountDown k  | k<=0 = [] 
-                | otherwise (2*k-1) : helper (2*k - 1) 
+oddCountDown k    | k<=0 = []
+    | otherwise = helper(2*k-1)
+helper :: Int -> [Int]
+helper 0 = []
+helper n    | n `mod` 2 == 0 = helper(n-1)
+    | otherwise = n : helper(n-1)
