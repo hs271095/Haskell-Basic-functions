81b38e067915db573470f58e91be861e76fc517d
Harshit <hs271095@gmail.com>
2019-09-23 02:16:20 -0400

    removeEven.hs updates

--- removeEven.hs
+++ removeEven.hs
@@ -2,7 +2,8 @@ removeEven ::[Int] -> [Int]
 removeEven [] = []
 removeEven (x:xs) = if (x `mod` 2) == 0 then x : removeEven(xs) else removeEven(xs)
 
--- removeEven ::[Int] -> [Int]
--- removeEven [] = []
--- removeEven (x:xs) | (x `mod` 2) == 0, x : removeEven(xs)
--- |  otherwise = removeEven(xs)
+--can also be written using gaurd conditions--
+removeEven2 ::[Int] -> [Int]
+removeEven2 [] = []
+removeEven2 (x:xs)   | (x `mod` 2) == 0 = x : removeEven2(xs)
+   | otherwise = removeEven2 (xs)
